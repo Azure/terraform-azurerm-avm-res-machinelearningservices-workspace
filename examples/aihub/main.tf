6400c9e4cc351de5e4bc72e07efa91cdf4fcc40a
Mohammad Oloomi <smh.oloomi@gmail.com>
2024-06-18 14:20:58 +0000

    Add AML managed vnet example with kind set to hub

---
+++ examples/aihub/main.tf
@@ -0,0 +1,31 @@
+data "azurerm_client_config" "current" {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
+      version = "1.13.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.prefix}mvnetrg"
+  location = var.location
+}
+
+resource "random_integer" "suffix" {
+  min = 10000000
+  max = 99999999
+}

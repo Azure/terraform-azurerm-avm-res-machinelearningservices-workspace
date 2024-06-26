c3177ec9d27c0ca876b6b972c8df6b67f2643859
Hannah Spencer <hannahlspencer@gmail.com>
2024-06-26 13:36:29 +0100

    More

--- main.vnet.tf
+++ main.vnet.tf
@@ -11,11 +11,12 @@ module "avm_res_network_virtualnetwork" {
 
   tags = var.tags
 
-  count = var.vnet == null == 0 ? 1 : length(var.vnet.resource_id) ? 1 : 0
+  count = var.vnet == null ? 0 : can(length(var.vnet.resource_id)) && length(var.vnet.resource_id) > 0 ? 1 : 0
+
 }
 
 data "azurerm_subnet" "shared" {
-  name                 = var.vnet.subnets[0].name
-  resource_group_name  = var.vnet == null ? var.resource_group.name : length(var.vnet.resource_id) == 0 ? var.resource_group.name : var.vnet.resource_group_name
+  name                 = var.vnet == null ? null : var.vnet.subnets[0].name
+  resource_group_name  = var.vnet == null ? var.resource_group.name : can(length(var.vnet.resource_id)) && length(var.vnet.resource_id) == 0 ? var.resource_group.name : var.vnet.resource_group_name
   virtual_network_name = local.vnet_name
 }

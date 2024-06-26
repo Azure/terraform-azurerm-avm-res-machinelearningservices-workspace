a73afac0b2e42147d8ad4d449c20d0ffbb5b66d0
Hannah Spencer <hannahlspencer@gmail.com>
2024-06-26 13:08:33 +0100

    Fix conditional again

--- locals.vnet.tf
+++ locals.vnet.tf
@@ -1,3 +1,3 @@
 locals {
-  vnet_name = var.vnet == null || length(var.vnet.resource_id) == 0 ? replace("vn${var.name}", "/[^a-zA-Z0-9-]/", "") : regex("(?<=/)[^/]+$", var.vnet.resource_id)
+  vnet_name = var.vnet == null ? replace("vn${var.name}", "/[^a-zA-Z0-9-]/", "") : length(var.vnet.resource_id) == 0 ? replace("vn${var.name}", "/[^a-zA-Z0-9-]/", "") : regex("(?<=/)[^/]+$", var.vnet.resource_id)
 }

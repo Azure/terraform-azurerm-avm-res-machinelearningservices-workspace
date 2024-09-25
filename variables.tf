cdd994fe4606a58103f34d6b53b43f63e16233f5
ritesh.modi <rimod@microsoft.com>
2024-09-25 08:56:26 +0100

    added default to variable outbound_rules

--- variables.tf
+++ variables.tf
@@ -19,8 +19,8 @@ variable "outbound_rules" {
     resource_id = string
     type        = string
   }))
-  description   = "Map of outbound rules for the managed network"
-  default       = null
+  description = "Map of outbound rules for the managed network"
+  default     = null
 }
 
 # This is required for most resource modules

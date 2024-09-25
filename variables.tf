e9aee29bf540c35d00afe8cb3edcd98e6a8e1928
Cara MacLaughlin <cmaclaughlin@microsoft.com>
2024-09-24 19:21:11 -0500

    Add diagnostic settings with example
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>

--- variables.tf
+++ variables.tf
@@ -76,7 +76,7 @@ variable "application_insights" {
     create_new = false
   }
   description = <<DESCRIPTION
-An object describing the Application Insights resource to create or use. This includes the following properties:
+An object describing the Application Insights resource to create or use for monitoring inference endpoints. This includes the following properties:
 - `resource_id` - (Optional) The resource ID of an existing Application Insights resource.
 - `create_new` - A flag indicating if a new resource must be created.
 - `tags` - (Optional) Tags for a new Application Insights resource.
@@ -138,7 +138,6 @@ variable "create_compute_instance" {
 }
 
 # required AVM interfaces
-# remove only if not supported by the resource
 # tflint-ignore: terraform_unused_declarations
 variable "customer_managed_key" {
   type = object({
@@ -160,6 +159,53 @@ A map describing customer-managed keys to associate with the resource. This incl
 DESCRIPTION  
 }
 
+# required AVM interface
+variable "diagnostic_settings" {
+  type = map(object({
+    name                                     = optional(string, null)
+    log_categories                           = optional(set(string), [])
+    log_groups                               = optional(set(string), ["allLogs"])
+    metric_categories                        = optional(set(string), ["AllMetrics"])
+    log_analytics_destination_type           = optional(string, "Dedicated")
+    workspace_resource_id                    = optional(string, null)
+    storage_account_resource_id              = optional(string, null)
+    event_hub_authorization_rule_resource_id = optional(string, null)
+    event_hub_name                           = optional(string, null)
+    marketplace_partner_resource_id          = optional(string, null)
+  }))
+  default     = {}
+  description = <<DESCRIPTION
+  A map of diagnostic settings to create on the Azure Machine Learning Workspace. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+  
+  - `name` - (Optional) The name of the diagnostic setting. One will be generated if not set, however this will not be unique if you want to create multiple diagnostic setting resources.
+  - `log_categories` - (Optional) A set of log categories to send to the log analytics workspace. Defaults to `[]`.
+  - `log_groups` - (Optional) A set of log groups to send to the log analytics workspace. Defaults to `["allLogs"]`.
+  - `metric_categories` - (Optional) A set of metric categories to send to the log analytics workspace. Defaults to `["AllMetrics"]`.
+  - `log_analytics_destination_type` - (Optional) The destination type for the diagnostic setting. Possible values are `Dedicated` and `AzureDiagnostics`. Defaults to `Dedicated`.
+  - `workspace_resource_id` - (Optional) The resource ID of the log analytics workspace to send logs and metrics to.
+  - `storage_account_resource_id` - (Optional) The resource ID of the storage account to send logs and metrics to.
+  - `event_hub_authorization_rule_resource_id` - (Optional) The resource ID of the event hub authorization rule to send logs and metrics to.
+  - `event_hub_name` - (Optional) The name of the event hub. If none is specified, the default event hub will be selected.
+  - `marketplace_partner_resource_id` - (Optional) The full ARM resource ID of the Marketplace resource to which you would like to send Diagnostic LogsLogs.
+  DESCRIPTION
+  nullable    = false
+
+  validation {
+    condition     = alltrue([for _, v in var.diagnostic_settings : contains(["Dedicated", "AzureDiagnostics"], v.log_analytics_destination_type)])
+    error_message = "Log analytics destination type must be one of: 'Dedicated', 'AzureDiagnostics'."
+  }
+  validation {
+    condition = alltrue(
+      [
+        for _, v in var.diagnostic_settings :
+        v.workspace_resource_id != null || v.storage_account_resource_id != null || v.event_hub_authorization_rule_resource_id != null || v.marketplace_partner_resource_id != null
+      ]
+    )
+    error_message = "At least one of `workspace_resource_id`, `storage_account_resource_id`, `marketplace_partner_resource_id`, or `event_hub_authorization_rule_resource_id`, must be set."
+  }
+}
+
+# required AVM interface
 variable "enable_telemetry" {
   type        = bool
   default     = true
@@ -236,6 +282,7 @@ DESCRIPTION
   }
 }
 
+# required AVM interface
 variable "lock" {
   type = object({
     kind = string
@@ -255,6 +302,24 @@ DESCRIPTION
   }
 }
 
+# required AVM interface
+# tflint-ignore: terraform_unused_declarations
+variable "managed_identities" {
+  type = object({
+    system_assigned            = optional(bool, false)
+    user_assigned_resource_ids = optional(set(string), [])
+  })
+  default     = {}
+  description = <<DESCRIPTION
+  Controls the Managed Identity configuration on this resource. The following properties can be specified:
+  
+  - `system_assigned` - (Optional) Specifies if the System Assigned Managed Identity should be enabled.
+  - `user_assigned_resource_ids` - (Optional) Specifies a list of User Assigned Managed Identity resource IDs to be assigned to this resource.
+  DESCRIPTION
+  nullable    = false
+}
+
+# required AVM interface
 variable "private_endpoints" {
   type = map(object({
     name = optional(string, null)
@@ -309,6 +374,7 @@ DESCRIPTION
   nullable    = false
 }
 
+# required AVM interface
 variable "role_assignments" {
   type = map(object({
     role_definition_id_or_name             = string
@@ -373,6 +439,7 @@ DESCRIPTION
   }
 }
 
+# required AVM interface
 # tflint-ignore: terraform_unused_declarations
 variable "tags" {
   type        = map(string)

e751ef710d9dc81ed5a438c633dd4c6eca7e84d5
Cara MacLaughlin <22799195+cmaclaughlin@users.noreply.github.com>
2024-09-26 09:59:14 -0500

    Add diagnostic settings with example (#61)
    * Add diagnostic settings with example
    
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>
    
    * Adding additional private endpoints (#63)
    
    * added additional private endpoints
    
    * doc updates and lints
    
    * added default to variable outbound_rules
    
    * added default to variable outbound_rules
    
    * fixing doc lints
    
    * removed locals for outbound rules
    
    * fix in documentation
    
    * fix in documentation
    
    * pre-commit on merged code
    
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>
    
    ---------
    
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>
    Co-authored-by: Ritesh Modi <rimod@microsoft.com>

---
+++ examples/diagnostic_settings/main.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.115.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = var.location
+  name     = module.naming.resource_group.name_unique
+}
+
+# This is the module call
+# Do not specify location here due to the randomization above.
+# Leaving location as `null` will cause the module to use the resource group location
+# with a data source.
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_storage_account" "example" {
+  account_replication_type = "ZRS"
+  account_tier             = "Standard"
+  location                 = azurerm_resource_group.this.location
+  name                     = module.naming.storage_account.name_unique
+  resource_group_name      = azurerm_resource_group.this.name
+}
+
+resource "azurerm_key_vault" "example" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.key_vault.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+}
+
+resource "azurerm_container_registry" "example" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.container_registry.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "Premium"
+}
+
+resource "azurerm_application_insights" "example" {
+  application_type    = "web"
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.application_insights.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  workspace_id        = azurerm_log_analytics_workspace.example.id
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.log_analytics_workspace.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_log_analytics_workspace" "diag" {
+  location            = azurerm_resource_group.this.location
+  name                = "diag${module.naming.log_analytics_workspace.name_unique}"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+module "azureml" {
+  source = "../../"
+  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
+  # ...
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.machine_learning_workspace.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+
+
+  storage_account = {
+    resource_id = azurerm_storage_account.example.id
+    create_new  = false
+  }
+
+  key_vault = {
+    resource_id = replace(azurerm_key_vault.example.id, "Microsoft.KeyVault", "Microsoft.Keyvault")
+    create_new  = false
+  }
+
+  container_registry = {
+    resource_id = azurerm_container_registry.example.id
+    create_new  = false
+  }
+
+  application_insights = {
+    resource_id = replace(azurerm_application_insights.example.id, "Microsoft.Insights", "Microsoft.insights")
+    create_new  = false
+  }
+
+  diagnostic_settings = {
+    diag = {
+      name                  = "aml${module.naming.monitor_diagnostic_setting.name_unique}"
+      workspace_resource_id = azurerm_log_analytics_workspace.diag.id
+    }
+  }
+
+  tags             = {}
+  enable_telemetry = var.enable_telemetry
+}
---
+++ main.diagnostics.tf
@@ -0,0 +1,40 @@
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  for_each = var.diagnostic_settings
+
+  name                           = each.value.name != null ? each.value.name : "diag-${var.name}"
+  target_resource_id             = local.aml_resource.id
+  eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
+  eventhub_name                  = each.value.event_hub_name
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  log_analytics_workspace_id     = each.value.workspace_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
+  storage_account_id             = each.value.storage_account_resource_id
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_categories
+
+    content {
+      category = enabled_log.value
+    }
+  }
+  dynamic "enabled_log" {
+    for_each = each.value.log_groups
+
+    content {
+      category_group = enabled_log.value
+    }
+  }
+  dynamic "metric" {
+    for_each = each.value.metric_categories
+
+    content {
+      category = metric.value
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      log_analytics_destination_type
+    ]
+  }
+}

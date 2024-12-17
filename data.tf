3008af04361963a2b0574aade4991278203d9b36
Cara MacLaughlin <cmaclaughlin@microsoft.com>
2024-12-17 11:06:15 -0600

    Remove unused code
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>

--- data.tf
+++ data.tf
@@ -10,13 +10,3 @@ data "azurerm_key_vault_key" "cmk" {
   key_vault_id = var.customer_managed_key.key_vault_resource_id
   name         = var.customer_managed_key.key_name
 }
-
-# tflint-ignore: terraform_comment_syntax
-data "azurerm_user_assigned_identity" "cmk" {
-  count = var.customer_managed_key == null || var.customer_managed_key.user_assigned_identity == null ? 0 : 1
-
-  name = try(
-    provider::azapi::parse_resource_id("Microsoft.ManagedIdentity/userAssignedIdentities", var.customer_managed_key.user_assigned_identity.resource_id),
-  "")
-  resource_group_name = var.resource_group_name
-}

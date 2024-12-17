cc946a5afa3c0883615256358e52e3b595cbbd6e
Cara MacLaughlin <cmaclaughlin@microsoft.com>
2024-12-16 18:21:09 -0600

    lint fix?
    Signed-off-by: Cara MacLaughlin <cmaclaughlin@microsoft.com>

--- data.tf
+++ data.tf
@@ -11,13 +11,12 @@ data "azurerm_key_vault_key" "cmk" {
   name         = var.customer_managed_key.key_name
 }
 
-locals {
-  cmk_user = var.customer_managed_key == null || var.customer_managed_key.user_assigned_identity == null ? null : provider::azapi::parse_resource_id("Microsoft.ManagedIdentity/userAssignedIdentities", var.customer_managed_key.user_assigned_identity.resource_id)
-}
-
+# tflint-ignore: terraform_comment_syntax
 data "azurerm_user_assigned_identity" "cmk" {
   count = var.customer_managed_key == null || var.customer_managed_key.user_assigned_identity == null ? 0 : 1
 
-  name                = local.cmk_user.name
+  name = try(
+    provider::azapi::parse_resource_id("Microsoft.ManagedIdentity/userAssignedIdentities", var.customer_managed_key.user_assigned_identity.resource_id),
+  "")
   resource_group_name = var.resource_group_name
 }

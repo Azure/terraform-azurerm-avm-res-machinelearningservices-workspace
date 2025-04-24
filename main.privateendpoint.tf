8bf32cc79d6f952982960dfb3d17e05ba4f9034e
Benjamin Dassow <benjamin.dassow@glueckkanja.com>
2025-04-24 12:28:22 +0200

    feat: add support for unmanaged DNS zone groups and role assignments for private endpoints

--- main.privateendpoint.tf
+++ main.privateendpoint.tf
@@ -34,9 +34,57 @@ resource "azurerm_private_endpoint" "this" {
   }
 }
 
+# The PE resource when we are managing **not** the private_dns_zone_group block:
+resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
+  for_each = { for k, v in var.private_endpoints : k => v if !var.private_endpoints_manage_dns_zone_group }
+
+  location                      = each.value.location != null ? each.value.location : var.location
+  name                          = each.value.name != null ? each.value.name : "pe-${var.name}"
+  resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
+  subnet_id                     = each.value.subnet_resource_id
+  custom_network_interface_name = each.value.network_interface_name
+  tags                          = each.value.tags
+
+  private_service_connection {
+    is_manual_connection           = false
+    name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
+    private_connection_resource_id = local.aml_resource.id
+    subresource_names              = ["amlworkspace"]
+  }
+  dynamic "ip_configuration" {
+    for_each = each.value.ip_configurations
+
+    content {
+      name               = ip_configuration.value.name
+      private_ip_address = ip_configuration.value.private_ip_address
+      member_name        = "default"
+      subresource_name   = "amlworkspace"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
+
+
 resource "azurerm_private_endpoint_application_security_group_association" "this" {
   for_each = local.private_endpoint_application_security_group_associations
 
   application_security_group_id = each.value.asg_resource_id
   private_endpoint_id           = azurerm_private_endpoint.this[each.value.pe_key].id
+}
+
+resource "azurerm_role_assignment" "private_endpoint" {
+  for_each = local.pe_role_assignments
+
+  principal_id                           = each.value.role_assignment.principal_id
+  scope                                  = try(azurerm_private_endpoint.this[each.value.private_endpoint_key].id, azurerm_private_endpoint.this_unmanaged_dns_zone_groups[each.value.private_endpoint_key].id)
+  condition                              = each.value.role_assignment.condition
+  condition_version                      = each.value.role_assignment.condition_version
+  delegated_managed_identity_resource_id = each.value.role_assignment.delegated_managed_identity_resource_id
+  principal_type                         = each.value.role_assignment.principal_type
+  role_definition_id                     = strcontains(lower(each.value.role_assignment.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_assignment.role_definition_id_or_name : null
+  role_definition_name                   = strcontains(lower(each.value.role_assignment.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_assignment.role_definition_id_or_name
+  skip_service_principal_aad_check       = each.value.role_assignment.skip_service_principal_aad_check
 }

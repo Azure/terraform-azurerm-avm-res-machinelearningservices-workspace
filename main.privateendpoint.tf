6aa1fe2a2fab32e7dc91eece7a3062134d30cb57
Benjamin Dassow <benjamin.dassow@glueckkanja.com>
2025-05-13 13:35:48 +0200

    refactor: remove unused locals for private endpoint application security group associations

--- main.privateendpoint.tf
+++ main.privateendpoint.tf
@@ -75,20 +75,6 @@ resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
   }
 }
 
-# Private endpoint application security group associations.
-# We merge the nested maps from private endpoints and application security group associations into a single map.
-locals {
-  private_endpoint_application_security_group_associations = { for assoc in flatten([
-    for pe_k, pe_v in var.private_endpoints : [
-      for asg_k, asg_v in pe_v.application_security_group_associations : {
-        asg_key         = asg_k
-        pe_key          = pe_k
-        asg_resource_id = asg_v
-      }
-    ]
-  ]) : "${assoc.pe_key}-${assoc.asg_key}" => assoc }
-}
-
 resource "azurerm_private_endpoint_application_security_group_association" "this" {
   for_each = local.private_endpoint_application_security_group_associations
 
